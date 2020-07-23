98976948f6c8c2c9bb9be3e5502db5d30ce817c8
Siwan19 <68042209+Siwan19@users.noreply.github.com>
2020-07-24 09:22:24 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    led.unplot(pos_X, pos_Y)
+    pos_X += 1
+    if (pos_X <= -1) {
+        pos_X = 4
+    }
+    led.plot(pos_X, pos_Y)
+})
+input.onButtonPressed(Button.B, function () {
+    led.unplot(pos_X, pos_Y)
+    pos_X += 1
+    if (pos_Y >= 4) {
+        pos_X = 0
+    }
+    led.plot(pos_X, pos_Y)
+})
+let pos_Y = 0
+let pos_X = 0
+pos_X = 0
+let direction = 0
+basic.forever(function () {
+    basic.pause(500)
+    led.unplot(pos_X, pos_Y)
+    pos_Y += direction
+    led.plot(pos_X, pos_Y)
+    if (pos_Y >= 4) {
+        direction = -1
+    } else if (pos_Y <= 0) {
+        direction = 1
+    }
+})
